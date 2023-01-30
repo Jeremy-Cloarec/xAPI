7ae28a42b3a6e2043fd989ddc9c47b5d2751cfe5
Cloarec <jeremycloarec@msn.com>
2023-01-30 17:58:29 +0100

    First commit

---
+++ xAPIStatement.js
@@ -0,0 +1,17 @@
+
+
+{
+    //create a JSON object with the bracket
+
+    //'key':'value'
+
+    "name" : "John",
+    "age" : 26,
+    "hobby" : { 
+        //an object in a bigger object
+        "winter_hobby" :"skiing",
+        "summer_hobby" : "swimming"
+
+    }
+
+}
